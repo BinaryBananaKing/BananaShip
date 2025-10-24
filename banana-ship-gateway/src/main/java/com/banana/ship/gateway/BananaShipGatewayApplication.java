a75ff60700d24620b479a5adea13bccad59391fb
tiankai1 <18302936163@163.com>
2025-10-24 23:55:56 +0800

    初始化

---
+++ banana-ship-gateway/src/main/java/com/banana/ship/gateway/BananaShipGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.banana.ship.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaShipGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BananaShipGatewayApplication.class, args);
+    }
+
+}
